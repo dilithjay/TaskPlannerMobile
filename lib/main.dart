44bb887d08c783cdcae22ba5b75a0a41f8aa8f1a
dilithjay <dilithjay@gmail.com>
2021-02-05 13:48:00 +0530

    Changed color scheme and added logo

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Task Planner',
-      theme: ThemeData(primarySwatch: Colors.teal),
+      theme: ThemeData(primarySwatch: Colors.lightBlue),
       home: TaskList(),
     );
   }
--- lib/task_list.dart
+++ lib/task_list.dart
@@ -54,7 +54,7 @@ class _TaskListState extends State<TaskList> {
           BottomNavigationBarItem(icon: Icon(Icons.view_day), label: "Daily"),
         ],
         currentIndex: _selectedIndex,
-        selectedItemColor: Colors.greenAccent,
+        selectedItemColor: Colors.lightBlue,
         onTap: _onItemTapped,
       ),
     );
